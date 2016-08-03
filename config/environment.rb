dc0160f49ce2fce266207edadf0472e6d6363470
Tomas Olivares <tolivares@gmail.com>
2016-08-03 09:32:21 +0200

    Changed environment variables for docker deployment

--- config/environment.rb
+++ config/environment.rb
@@ -108,7 +108,7 @@ Rails::Initializer.run do |config|
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
     :session_key => '_yourflightlog_session',
-    :secret      => '22ee4fe1729aaf98e2c040be1429583c0cfa437a6c616fdc3227807acf664f67d4241dc98b9f0b9ae9ff11e9c76b0a624f63bb6a299c6a501a1190e8f1240fa5w'
+    :secret      => ENV['SECRET_TOKEN']
   }
 
   #config.action_controller.relative_url_root = ''
